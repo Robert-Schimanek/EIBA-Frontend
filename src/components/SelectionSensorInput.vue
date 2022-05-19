e68c1789a6b748d9b7a42301b01990e95025e5c3
b-petz <barbarella.petz@gmail.com>
2022-05-19 17:34:30 +0200

    Added SelectionSensorInput.vue to Components

---
+++ src/components/SelectionSensorInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form @submit.prevent='SelectionSensorInput'>
+    <div>
+        <label for="box">Put core on center of scale and remove your hands from core</label>
+    </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SelectionSensorInput',
+  data() {
+    return {
+      box: ''
+      form_data: {
+        bar_code_scanable: 'Y',
+        box_exists: 'Y',
+      },
+      bde_server_start_response: ''
+      }
+    };
+  // emits: ['change-evaluation'],
+  methods: {
+  },
+};
+
+</script>
+
+<style>
+
+</style>
