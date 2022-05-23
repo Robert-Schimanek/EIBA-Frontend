017aa3d8321ffd6845a3d610f37f7ec286cc36e8
xxSmuffixx <52321608+xxSmuffixx@users.noreply.github.com>
2022-05-23 21:49:18 +0200

    eslintrc Settings exported to own file
    With that setting the linebreaks should work for windows, as well for Unix

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+var OFF = 0, WARN = 1, ERROR = 2;
+
+module.exports = exports = {
+    "root": true,
+    "env": {
+        "node": true
+    },
+    "extends": [
+        "plugin:vue/vue3-essential",
+        "@vue/airbnb"
+    ],
+    "parserOptions": {
+        "parser": "babel-eslint"
+    },
+    "rules": {
+        "linebreak-style": ["error", (process.platform === "win32" ? "windows" : "unix")]
+    }
+};
