d6e4ca3f3e777984701939801ac4c43dfd8782a9
Robert Schimanek <robert.schimanek@icloud.com>
2022-05-24 20:33:29 +0200

    Initial SelectionResultOEN
    Initial SelectionResultOEN

---
+++ src/components/SelectionResultOEN.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <p>Selection ID: {{ form_data.temp_selection_id }}</p>
+    <p>Core Mass: {{ form_data.core_mass }}</p>
+    <p>Method: {{ method }}</p>
+    <form @submit.prevent='SelectionResultOEN(form_data.temp_selection_id)'>
+      <button>
+      Get results
+      </button>
+    </form>
+  </div>
+  <div>
+    <p>Product Group Prediction list:
+    {{ bde_server_result_OEN_response.product_group_prediction_list}}</p>
+    <p>OEN Prediction list: {{ bde_server_result_OEN_response.oen_prediction_list}}</p>
+    <p>ProductGroup prediction has started:
+    {{ bde_server_result_OEN_response.prediction_product_group_commissioned }}</p>
+    <p>Temporary selection id: {{ bde_server_result_OEN_response.temp_selection_id }}</p>
+  </div>
+  <div v-if="form_data.temp_selection_id==bde_server_result_OEN_response.temp_selection_id">
+    <button
+      @click="changeEvaluation(bde_server_result_OEN_response.temp_selection_id)">
+        Put core on scale
+    </button>
+  </div>
+  <ul id="array-rendering">
+    <li v-for="item in boxlinks" :key="item">{{ item.box_code }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'SelectionResultOEN',
+  props: ['selection_id_main', 'core_mass_main'],
+  data() {
+    return {
+      form_data: {
+        core_mass: this.core_mass_main,
+        temp_selection_id: this.selection_id_main,
+      },
+      bde_server_result_OEN_response: '',
+    };
+  },
+  methods: {
+    SelectionResultOEN(selectionIdResult) {
+      this.$axios.get(`http://localhost:5100/bde/selection/evaluation/status/${encodeURIComponent(selectionIdResult)}`)
+        .then((response) => { this.bde_server_result_OEN_response = response.data; });
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
