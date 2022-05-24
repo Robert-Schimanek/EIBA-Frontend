cae06df16da233f39e98c2d405571ccf8cfd9db1
Robert Schimanek <robert.schimanek@icloud.com>
2022-05-24 17:52:35 +0200

    Update button @click check prediction model
    Update button @click check prediction model

--- src/components/SelectionStart.vue
+++ src/components/SelectionStart.vue
@@ -34,10 +34,11 @@
         <p>Each inspection is a unique process.
         Generate a unique identifier (Selection ID) to this operation to initialize
         the operation in the system.</p>
-        <!-- <button @click="form_data.temp_selection_id=generateID()">Generate ID</button> -->
-        <p>Selection ID: {{form_data.temp_selection_id=generateID()}}</p>
+        <!-- <button @click="form_data.temp_selection_id=generateID()">Generate ID</button>-->
+        <p>Selection ID: {{random_ID}}</p>
       </div>
-      <button @click="form_data.bar_code = box.bar_code">Check prediction models</button>
+      <button @click="form_data.temp_selection_id=generateID();form_data.bar_code = box.bar_code">
+      Check prediction models</button>
       <button>No Box</button>
       <button>No Barcode</button>
     </form>
