2fa7df067611358b1d218017b7281f6ac41b6ea1
b-petz <barbarella.petz@gmail.com>
2022-05-24 13:29:33 +0200

    Commented SelectionSensorInput out

--- src/views/Selection.vue
+++ src/views/Selection.vue
@@ -12,25 +12,24 @@
     @change-evaluation="change($event)"
     @selection-id="sendselectionidtochild($event)"/>
   </keep-alive>
-  <keep-alive>
+  <!-- <keep-alive>
     <selection-start
     v-if="aSelTab === 'SelectionSensorInput'"
     :selection_id_main="selection_id_main"/>
-  </keep-alive>
+  </keep-alive> -->
   </div>
 </template>
 
 <script>
 import SelectionStart from '../components/SelectionStart.vue';
 import SelectionEvaluation from '../components/SelectionEvaluation.vue';
-import SelectionEvaluation from '../components/SelectionSensorInput.vue';
 
 export default {
   name: 'Selection',
   components: {
     SelectionStart,
     SelectionEvaluation,
-    SelectionSensorInput
+    // SelectionSensorInput
   },
   data() {
     return {
