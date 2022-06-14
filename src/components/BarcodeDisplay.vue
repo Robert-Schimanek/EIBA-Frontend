943dc1b7bd5e98a3116e0e58ef1cb6afdaf87f73
b-petz <barbarella.petz@gmail.com>
2022-06-14 16:18:51 +0200

    Added new component BarcodeDisplay.vue to generate barcode, but does not work, yet

---
+++ src/components/BarcodeDisplay.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="bar_code">
+    <VueBarcode  v-bind:value="bar_code" :options="{ displayValue: true }"
+    style="width: 170px">
+    </VueBarcode>
+  </div>
+</template>
+
+<script type="vue-template" id="barcode-display">
+import VueBarcode from '@chenfengyuan/vue-barcode';
+
+export default {
+  components: {
+    VueBarcode,
+  },
+  props: {
+    bar_code: { default: '' },
+  },
+  data() {
+    return {
+      bar_code_i: this.bar_code,
+    };
+  },
+  methods: {
+  },
+
+};
+</script>
+<style src="../assets/styles/styles.css"></style>
