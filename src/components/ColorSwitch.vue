933a45909ce920a9b6a30f6e90e1d105abc255ba
b-petz <barbarella.petz@gmail.com>
2022-07-14 15:04:14 +0200

    Implemented Checkbox to ColorSwitch.vue

---
+++ src/components/ColorSwitch.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="toggle-wrapper">
+    <label class="toggle">
+        <input type="checkbox"
+        :checked="(mode === 'dark') ? 'checked' : false"
+        @change="$emit('toggle')"/>
+        <span class="toggler round"></span>
+        </label>
+        </div>
+        </template>
+
+<script>
+</script>
+
+<style>
+</style>
